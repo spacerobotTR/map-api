f46029a3d5a810f40efb5d47fa57cea9dd5ff885
Chris Stewart <siriiven@gmail.com>
2018-10-20 10:46:24 -0400

    Maps functioning

--- app/javascript/packs/maps/index.js
+++ app/javascript/packs/maps/index.js
@@ -1,1 +1,27 @@
-console.log("maps api")
+document.addEventListener("turbolinks:load", function() {
+
+  var map = new GMaps({
+    div: '#map',
+    lat: 43.036302641877,
+    lng: -76.1308899542609,
+    gestureHandling: 'greedy'
+  });
+  window.map = map;
+
+  var incidents = JSON.parse(document.querySelector("#map").dataset.incidents);
+  window.incidents = incidents;
+
+  incidents.forEach(function(incident) {
+    if (incident.latitude && incident.longitude) {
+      var marker = map.addMarker({
+        lat: incident.latitude,
+        lng: incident.longitude,
+        title: incident.address,
+        infoWindow: {
+          content: `<p><a href='/incidents/${incident.id}'>${incident.address}</a></p>`
+        }
+      });
+    }
+  });
+
+});
